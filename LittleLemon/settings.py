dfaa3ba0cccfe88ea4f3618362d42e4955a6fe25
Thomas <thoward3344@gmail.com>
2023-12-11 14:08:23 -0500

    Setup of first database models in restaurant app in Django

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = "LittleLemon.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "LittleLemon",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "USER": "root",
+        "PASSWORD": "kiba3344",
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
