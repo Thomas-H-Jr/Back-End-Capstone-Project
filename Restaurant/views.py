ef7eb0ffa1ce2104815ee5bf165e322f946b74bb
Thomas <thoward3344@gmail.com>
2023-12-11 16:32:56 -0500

    Setup of the menu and table booking api for the Little Lemon back end using DRF(Django Rest Framework). Also, changes to the settings.py file.

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Booking, Menu
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.contrib.auth.models import User
 
 
 # Create your views here.
-def index(request):
-    return render(request, "index.html", {})
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
