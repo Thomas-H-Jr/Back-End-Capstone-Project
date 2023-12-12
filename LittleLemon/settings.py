54be26106d947b7a99c7e1f67aa299a54392a521
Thomas <thoward3344@gmail.com>
2023-12-12 13:46:31 -0500

    Updated notes.txt with usernames, passwords, and tokens in the project folder . Also, flushed mysql privileges so anyone can access the database. MySQL database info is in settings.py

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "HOST": "127.0.0.1",
         "PORT": "3306",
         "USER": "root",
-        "PASSWORD": "kiba3344",
+        "PASSWORD": "",
         "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
