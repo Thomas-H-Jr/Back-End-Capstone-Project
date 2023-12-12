154eb5f6cc66b9c7d9f664bd741c609c7954bfae
Thomas <thoward3344@gmail.com>
2023-12-12 14:16:36 -0500

    added index view back into the project

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from . import views
 from .views import MenuItemsView, SingleMenuItemView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("api-token-auth/", obtain_auth_token),
+    path("", views.index, name="index"),
     path("items/", MenuItemsView.as_view()),
     path("items/<int:pk>", SingleMenuItemView.as_view()),
     # path("booking/", views.BookingView.as_view()),
